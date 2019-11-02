9377a5ff7732cdf57da070ee4dcabd504c704447
Aeden Jameson <aeden.jameson@gmail.com>
2019-11-02 14:19:14 -0700

    basic upd server working

--- src/main.go
+++ src/main.go
@@ -1,9 +1,53 @@
 package main
 
 import (
-	"fmt"
+  "fmt"
+  "net"
+  "log"
+  "math/rand"
+  "time"
+  "strings"
+  "strconv"
+  "github.com/aedenj/golang-tftp/tftp"
 )
 
+func random(min, max int) int {
+  return rand.Intn(max-min) + min
+}
+
 func main() {
-	fmt.Println("Hello World")
+  s, err := net.ResolveUDPAddr("udp", "0.0.0.0:3000")
+  if err != nil {
+    log.Fatal(err)
+    return
+  }
+
+  conn, err := net.ListenUDP("udp", s)
+  if err != nil {
+    log.Fatal(err)
+    return
+  }
+  defer conn.Close()
+
+
+  buffer := make([]byte, tftp.MaxPacketSize)
+  rand.Seed(time.Now().Unix())
+
+  for {
+    n, addr, err := conn.ReadFromUDP(buffer)
+    fmt.Print("-> ", string(buffer[0:n-1]))
+
+    if strings.TrimSpace(string(buffer[0:n])) == "STOP" {
+      fmt.Println("Exiting UDP server!")
+      return
+    }
+
+    data := []byte(strconv.Itoa(random(1, 1001)))
+    fmt.Printf("data: %s\n", string(data))
+    _, err = conn.WriteToUDP(data, addr)
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
+  }
 }
